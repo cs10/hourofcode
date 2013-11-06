e9243bd64afce5fc29ac15c01d038cfedf0bfbcc
Kyle Zentner <zentner.kyle@gmail.com>
2013-11-05 20:59:31 -0800

    Tweaked page styles, changed projects to xml files.

--- runme.js
+++ runme.js
@@ -1,14 +1,19 @@
+
+var snap_url = "http://snap.berkeley.edu/snapsource/snap.html";
+//var snap_url = "snap.html";
+
+
 $(document).ready(function() {
 
 
-	load('Dan Garcia', 'Mole Mash');
-
+        //load('Dan Garcia', 'Mole Mash');
 });
 
-
-
+function load_project_xml(uri) {
+        document.getElementById('snap').contentWindow.load_project_xml(uri);
+}
 
 function load(user, proj) {
-	document.getElementById('snap').src = 'http://snap.berkeley.edu/snapsource/snap.html#present:Username=' + user + '&ProjectName=' + proj;
+	document.getElementById('snap').src = snap_url + '#present:Username=' + user + '&ProjectName=' + proj;
 	$('#iframes').html($('#iframes').children().clone());
 }
